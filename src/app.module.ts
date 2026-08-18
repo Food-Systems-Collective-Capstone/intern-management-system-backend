0a5d8f7341056c68904e6cb14e4352853383a5bf
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-08-18 23:44:53 +1000

    feat: backend project structure setup and supabase

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+//AI GENERATED CODE BELOW
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService], // <-- Add AppService back here
+})
+export class AppModule {}
