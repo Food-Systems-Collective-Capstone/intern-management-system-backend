7ca04da3dafd5b54a0acaa3eb3152379a1ede391
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-08-24 17:33:22 +1000

    fix: specifying type for column output

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,18 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
 
+interface Columns {
+  column_name: string;
+  data_type: string;
+  is_nullable: string;
+}
+
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly dataSource: DataSource
-    ) {}
+    private readonly dataSource: DataSource,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -15,8 +21,10 @@ export class AppController {
   }
 
   @Get('health/db')
-  async testDb(){
-    const result = await this.dataSource.query("SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'person_profile';");
-    return { result }
+  async testDb(): Promise<{ result: Columns[] }> {
+    const result = await this.dataSource.query<Columns[]>(
+      "SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'person_profile';",
+    );
+    return { result };
   }
 }
