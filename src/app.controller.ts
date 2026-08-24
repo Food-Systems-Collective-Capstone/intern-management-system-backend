a6184baeb2b126b51e9fc1240f10b21071ee38f8
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-08-24 17:06:50 +1000

    test: verify database verify works for shared_accounts table

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DataSource } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly dataSource: DataSource
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health/db')
+  async testDb(){
+    const result = await this.dataSource.query("SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'person_profile';");
+    return { result }
   }
 }
