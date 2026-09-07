c367b538b51d71a13763e1d99baa81ea82c76ff3
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-07 17:05:48 +1000

    feat: added health check and db connection test endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataSource } from 'typeorm';
 
 describe('AppController', () => {
   let appController: AppController;
+  let dataSource: DataSource;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, {
+        provide: DataSource,
+        useValue: {query: jest.fn}
+      }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
