1e380fda769403f539fb20f1db6a25feac678b55
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-15 15:11:45 +1000

    feat: scaffold application module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { AppService } from './app.service';
         },
       }),
     }),
+    ApplicationsModule,
   ],
   controllers: [AppController],
   providers: [AppService], // <-- Add AppService back here
