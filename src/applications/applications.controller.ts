1fb45b155935d7afe99f26e8fada1454e65c5ce0
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-15 16:48:39 +1000

    fix: return type assignment to methods

--- src/applications/applications.controller.ts
+++ src/applications/applications.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { CreateApplicationDto } from './dto/create-application.dto';
+import { PersonProfile } from './interfaces/person-profile.interface';
 
 
 @Controller()
@@ -8,7 +9,7 @@ export class ApplicationsController {
     constructor (private readonly applicationService : ApplicationsService){}
 
     @Post('api/applications')
-    async create(@Body() createApplicationDto: CreateApplicationDto){
+    async create(@Body() createApplicationDto: CreateApplicationDto) : Promise<PersonProfile>{
         return this.applicationService.createApplication(createApplicationDto);
     }
 }
