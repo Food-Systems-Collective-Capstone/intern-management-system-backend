6ccb90c75186b4f50fc84a4041697cc087818e11
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-15 15:04:27 +1000

    fix: firstname, lastname to match database

--- supabase/migrations/create_accounts_profile.sql
+++ supabase/migrations/create_accounts_profile.sql
@@ -9,8 +9,8 @@ CREATE TABLE shared_accounts (
 
 CREATE TABLE person_profile(
     person_id UUID PRIMARY KEY REFERENCES shared_accounts(id) ON DELETE CASCADE,
-    firstName text NOT NULL,
-    lastNAme text NOT NULL,
+    firstname text NOT NULL,
+    lastname text NOT NULL,
     phone text NOT NULL UNIQUE,
     email text NOT NULL UNIQUE,
     university text NOT NULL,
