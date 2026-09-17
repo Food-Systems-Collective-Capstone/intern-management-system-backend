d00d3016a59b727a7f9f4149c42ee83d3d0b0671
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-17 22:56:41 +1000

    feat: added supabase env variables to constructor

--- src/supabase-storage-bucket/supabase-storage-bucket.service.ts
+++ src/supabase-storage-bucket/supabase-storage-bucket.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { createClient, SupabaseClient} from '@supabase/supabase-js';
 
 @Injectable()
-export class SupabaseStorageBucketService {}
+export class SupabaseStorageBucketService {
+    private readonly client: SupabaseClient;
+    private readonly bucketName: 'Resume';
+
+    constructor(){
+        this.client = createClient (
+            process.env.SUPABASE_URL!,
+            process.env.SUPABASE_KEY!,
+        )
+    };
+}
