a7c2d1cc254c0feca4582e01588b8f5ee60e4b97
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-18 20:44:08 +1000

    fix: fixing unit test code

--- src/applications/applications.controller.spec.ts
+++ src/applications/applications.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
+import { SupabaseStorageBucketService } from '../supabase-storage-bucket/supabase-storage-bucket.service';
 
 describe('ApplicationsController', () => {
   let controller: ApplicationsController;
@@ -11,8 +12,12 @@ describe('ApplicationsController', () => {
       providers: [
         {
           provide: ApplicationsService,
-          useValue: { CreateApplicationDto: jest.fn() },
+          useValue: { createApplication: jest.fn() },
         },
+        {
+          provide: SupabaseStorageBucketService,
+          useValue: {uploadResume: jest.fn()},
+        }
       ],
     }).compile();
 
--- src/supabase-storage-bucket/supabase-storage-bucket.service.spec.ts
+++ src/supabase-storage-bucket/supabase-storage-bucket.service.spec.ts
@@ -1,6 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupabaseStorageBucketService } from './supabase-storage-bucket.service';
 
+//Test code from line 5 to 11 is AI generated 
+jest.mock('@supabase/supabase-js', () => ({
+  createClient: jest.fn(() => ({
+    storage: {
+      from: jest.fn().mockReturnThis(),
+      upload: jest.fn(),
+    }
+  }))
+}))
+
 describe('SupabaseStorageBucketService', () => {
   let service: SupabaseStorageBucketService;
 
