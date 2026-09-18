d4ee5c6129b0103a719d956cc019615ba2d00d3d
SameerASiddiqui2006 <sameeralisiddiqui42@gmail.com>
2026-09-18 18:53:00 +1000

    feat: added upload resume method

--- src/supabase-storage-bucket/supabase-storage-bucket.service.ts
+++ src/supabase-storage-bucket/supabase-storage-bucket.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { createClient, SupabaseClient} from '@supabase/supabase-js';
 
 @Injectable()
-export class SupabaseStorageBucketService {
+export class SupabaseStorageBucketService{
     private readonly client: SupabaseClient;
-    private readonly bucketName: 'Resume';
+    private readonly bucketName = 'Resume';
 
     constructor(){
         this.client = createClient (
@@ -13,7 +13,18 @@ export class SupabaseStorageBucketService {
         )
     };
 
-    async uploadResume (){
-        
+    async uploadResume (file: Express.Multer.File, personid : string) : Promise<string>{
+        const filePath = `${personid}/applicantResume.pdf`;
+
+        const { error } = await this.client.storage .from(this.bucketName) .upload(filePath, file.buffer, {
+            contentType: file.mimetype,
+            upsert: true,
+        });
+
+        if (error){
+            throw new Error (`Upload has failed: ${error.message}`);
+        }
+
+        return filePath;
     }
 }
