4bb4e72e19a84063c9c2ad4535f5697328d058fb
SameerASiddiqui2006 <152408668+SameerASiddiqui2006@users.noreply.github.com>
2026-09-24 21:17:48 +1000

    Revert "Revert "Feat/get endpoint""

--- src/applications/applications.controller.spec.ts
+++ src/applications/applications.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
 import { SupabaseStorageBucketService } from '../supabase-storage-bucket/supabase-storage-bucket.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
 
 describe('ApplicationsController', () => {
   let controller: ApplicationsController;
@@ -18,8 +20,14 @@ describe('ApplicationsController', () => {
           provide: SupabaseStorageBucketService,
           useValue: { uploadResume: jest.fn() },
         },
+
       ],
-    }).compile();
+    })
+    .overrideGuard(AuthGuard('jwt'))
+    .useValue({ canActivate: () => true})
+    .overrideGuard(RolesGuard)
+    .useValue({ canActivate: () => true})
+    .compile()
 
     controller = module.get<ApplicationsController>(ApplicationsController);
   });
